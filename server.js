3a961d5819802a4e52bd4616e5f09da09bcf0983
prabal laad <praballaad17@gmail.com>
2021-05-20 11:51:19 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,87 @@
+const express = require("express");
+const passport = require('passport')
+const flash = require('express-flash')
+const session = require('express-session')
+const methodOverride = require('method-override')
+const app = express();
+const port = 3000;
+
+app.set("view-engine", "ejs");
+app.use(express.urlencoded({  extended: false }));
+
+
+const users = [
+    { id: '1621486389736', name: 'w', email: 'w@w', password: 'w' }
+];
+
+const initializePassport = require("./passport");
+initializePassport(
+  passport,
+  (email) => users.find((user) => user.email === email),
+  (id) => users.find((user) => user.id === id)
+);
+
+app.use(flash())
+app.use(session({
+  secret: "secret",
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(methodOverride('_method'))
+
+app.get("/", checkAuthenticated, (req, res) => {
+  res.render("index.ejs", { name: req.user.name });
+});
+app.get("/login", checkNotAuthenticated, (req, res) => {
+  res.render("login.ejs");
+});
+
+app.post("/login", checkNotAuthenticated,  passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true
+  }));
+
+  app.get("/register", checkNotAuthenticated, (req, res) => {
+    res.render("register.ejs");
+  });
+app.post("/register", checkNotAuthenticated, (req, res) => {
+    try {
+        users.push({
+            id: Date.now().toString(),
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password
+        })
+        res.redirect('/login')
+    } catch  {
+        res.redirect('/register')
+    }
+  console.log(users)
+});
+
+app.delete('/logout', (req, res) => {
+    req.logOut()
+    res.redirect('/login')
+  })
+
+  function checkAuthenticated(req, res, next) {
+      if(req.isAuthenticated()) {
+          return next()
+      }
+      res.redirect('/login')
+  }
+
+  function checkNotAuthenticated(req, res, next) {
+    if(req.isAuthenticated()) {
+        return res.redirect('/')
+    }
+    next()
+}
+
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
